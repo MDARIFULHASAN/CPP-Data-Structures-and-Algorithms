40884fea59b9db168f5c7e90ddd453364d03f4f3
MDARIFULHASAN <42723028+MDARIFULHASAN@users.noreply.github.com>
2024-11-02 22:31:56 -0400

    modified in_out.cpp to in_out_modified.cpp

---
+++ Chapter01/in_out_modified.cpp
@@ -0,0 +1,22 @@
+// in_out.cpp
+// If.cbp
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+ int i;
+ 
+ cout << "Please enter an integer value: ";
+ cin >> i;
+ cout << "The value you entered is " << i;
+
+if(i > 100)
+    cout << "greater than 100.";
+else
+    cout << "equals or less than 100.";
+cout << endl;
+
+ return 0;
+}
